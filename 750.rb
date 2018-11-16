b342c41ecde603a06853b09fff222c3528eb8410
KazukiHozumi <hozumikazuki.8140@gmail.com>
2018-11-17 01:41:53 +0900

    finish 750

--- 750.rb
+++ 750.rb
@@ -2,9 +2,17 @@ n = gets.to_i
 
 a = Array.new(n)
 b = Array.new(n)
+c = Array.new(n)
 
 n.times do |i|
   a[i], b[i] = gets.split.map{|x| x.to_i}
+  c[i] = i
 end
 
+b_times = b.inject(:*)
 
+c.sort_by!{|i| a[i] * b_times / b[i]}.reverse!
+
+c.each do |i|
+  puts "#{a[i]} #{b[i]}"
+end
