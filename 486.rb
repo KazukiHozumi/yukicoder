cd63a6bfd8c7b46cd3cb8a835cba5f0e9f88f911
KazukiHozumi <hozumikazuki.8140@gmail.com>
2018-11-30 01:54:36 +0900

    finish 486

--- 486.rb
+++ 486.rb
@@ -1,5 +1,19 @@
 s = gets.chomp.split('')
 
-s.each do |i|
+s.size.times do |i|
+  if i + 2 == s.size
+    break
+  end
 
+  if s[i] == s[i+1] && s[i] == s[i+2]
+    if s[i] == 'O'
+      puts 'East'
+      exit
+    else
+      puts 'West'
+      exit
+    end
+  end
 end
+
+puts 'NA'
