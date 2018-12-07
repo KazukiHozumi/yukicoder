e1b640cbb3ad349e850652c4973d012888a5afa1
KazukiHozumi <hozumikazuki.8140@gmail.com>
2018-12-08 02:06:34 +0900

    add 405

---
+++ 405.rb
@@ -0,0 +1,15 @@
+set = ['I', 'II', 'III', 'IIII', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII']
+
+s1, t = gets.split
+
+index = set.find_index(s1) + t.to_i
+
+if index >= 12
+  index -= 12
+end
+
+if index < 0
+  index -= index/12*12
+end
+
+puts set[index]
