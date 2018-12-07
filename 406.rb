361d83d2c8be0dcd4e342541279af5d66a3935fa
KazukiHozumi <hozumikazuki.8140@gmail.com>
2018-12-07 22:14:18 +0900

    finish 406

--- 406.rb
+++ 406.rb
@@ -3,7 +3,7 @@ n = gets.to_i
 x = gets.split.map(&:to_i).sort!
 
 (n-2).times do |i|
-  if x[i+1]-x[i] != x[i+2]-x[i+1]
+  if x[i+1]-x[i] != x[i+2]-x[i+1] || x[i+1] == x[i] || x[i+2] == x[i+1]
     puts 'NO'
     exit
   end
