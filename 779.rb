3d1008e357480999a5a295b627e204014778f465
KazukiHozumi <hozumikazuki.8140@gmail.com>
2019-02-04 23:22:50 +0900

    finish 779

--- 779.rb
+++ 779.rb
@@ -5,7 +5,7 @@ judge = Time.new(y, m, d)
 below_time = Time.new(1989, 1, 8)
 over_time = Time.new(2019, 4, 30)
 
-if judge >= below_time && judge < over_time
+if judge >= below_time && judge <= over_time
   puts 'Yes'
 else
   puts 'No'
