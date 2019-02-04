43db53d9ab96bd826178f4a8ffb8de21b42d97a3
KazukiHozumi <hozumikazuki.8140@gmail.com>
2019-02-04 22:43:35 +0900

    fix 779

--- 779.rb
+++ 779.rb
@@ -1,2 +1,12 @@
 y, m, d = gets.split.map(&:to_i)
 
+judge = Time.new(y, m, d)
+
+below_time = Time.new(1989, 1, 8)
+over_time = Time.new(2019, 4, 30)
+
+if judge >= below_time && judge < over_time
+  puts 'Yes'
+else
+  puts 'No'
+end
